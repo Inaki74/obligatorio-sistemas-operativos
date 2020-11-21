b7cc0d72270dd93f5ba1c03c61965c1d77d62d1c
Matias Gonzalez <matias.gonzalez.2407@gmail.com>
2020-11-21 14:08:57 -0300

    Descripcion de la logica de usuario en main, borrar despues de implementarlo

--- main.java
+++ main.java
@@ -24,4 +24,22 @@ class main{
         System.out.println(colores.ANSI_RED + "EXIT" + colores.ANSI_RESET);
     }
 
+
+    /**
+     * Logica de usuario:
+     * El usuario tiene permisos a procesos y recursos, para esto necesitamos
+     * Matriz de bools (True = acceso, False = no) de usuarios y recursos
+     * Matriz de bools "" de usuarios y procesos
+     * 
+     * Uso de las matrices:
+     * Cuando vas a ejecutar un proceso ver en la posicion M[u][p] si el usuario tiene permiso, si no tiene printear "No tiene acceso a este proceso" y seguir con el proximo en RR
+     * Cuando vas adentro de un proceso vas a PEDIR un recurso fijarse si el usuario tiene permiso para utilizar el recurso, Si lo tiene avanza normal, si no matar proceso
+     * 
+     * IDEAS:
+     * Funcion en sistema permisoRecurso(usuario, recurso)
+     * Funcion en sistema permisoProceso(usuario, proceso)
+     * Funcion en PCB matarProceso que lo termine y lo saque del scheduller
+     * Fijarse si usuario tiene permiso afuera del PCB, si no lo tiene no ejecutar el proceso directamente
+     * Fijarse si el usuario tiene permiso para usar el recurso, si no lo tiene, llamar a matarProceso, y sacar proceso de Round Robin 
+     */
 }
