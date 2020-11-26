c02887571dc4a116e26bf88e2c22cf31f4109c32
GLandeira <gastonlandeira@gmail.com>
2020-11-26 15:48:54 -0300

    More test cases

--- Clases/Sistema.java
+++ Clases/Sistema.java
@@ -45,10 +45,10 @@ public class Sistema{
         //importar Usuarios y cargarlos en procesos
         String[] importarUsuarios = {"Matixatim Admin"};
         for(int i=0; i<importarUsuarios.length; i++){
-            String usuario[] = importarUsuarios[i].split(" ");
-            boolean[] permisosRecursos = devolverPermisosRecursosCasoX(definirPerfil(usuario[1]));
-            boolean[] permisosProgramas = devolverPermisosProgramasCasoX(definirPerfil(usuario[1]));
-            Usuario newUsuario = new Usuario(usuario[0], permisosRecursos, permisosProgramas);
+            String stringUsuario[] = importarUsuarios[i].split(" ");
+            boolean[] permisosRecursos = devolverPermisosRecursosCasoX(definirPerfil(stringUsuario[1]));
+            boolean[] permisosProgramas = devolverPermisosProgramasCasoX(definirPerfil(stringUsuario[1]));
+            Usuario newUsuario = new Usuario(stringUsuario[0], permisosRecursos, permisosProgramas);
             usuarios.add(newUsuario);
         }
         procesos.get(0).setUsuario(usuarios.get(0));
@@ -83,12 +83,12 @@ public class Sistema{
         asignarAMemoria(proceso1);
         asignarAMemoria(proceso2);
         //importar Usuarios y cargarlos en procesos
-        String[] importarUsuarios = {"Matixatim Admin", "Inaki.exe User", "GL Guest", "Caffa Admin"};
+        String[] importarUsuarios = {"Matixatim Admin"};
         for(int i=0; i<importarUsuarios.length; i++){
-            String usuario[] = importarUsuarios[i].split(" ");
-            boolean[] permisosRecursos = devolverPermisosRecursosCasoX(definirPerfil(usuario[1]));
-            boolean[] permisosProgramas = devolverPermisosProgramasCasoX(definirPerfil(usuario[1]));
-            Usuario newUsuario = new Usuario(usuario[0], permisosRecursos, permisosProgramas);
+            String stringUsuario[] = importarUsuarios[i].split(" ");
+            boolean[] permisosRecursos = devolverPermisosRecursosCasoX(definirPerfil(stringUsuario[1]));
+            boolean[] permisosProgramas = devolverPermisosProgramasCasoX(definirPerfil(stringUsuario[1]));
+            Usuario newUsuario = new Usuario(stringUsuario[0], permisosRecursos, permisosProgramas);
             usuarios.add(newUsuario);
         }
         procesos.get(0).setUsuario(usuarios.get(0));
@@ -98,7 +98,297 @@ public class Sistema{
         inicializarGrafo();
     }
 
+    public void casosPruebaDeadlocks3(){
+        //Importar programas
+        String[][] importarProgs = {{"Pedir impresora#2","B","C","A","B","B", "Pedir impresora#1", "Usar impresora#2", "Usar impresora#1", "Devolver impresora#1", "Devolver impresora#2", "B", "A", "C"}, 
+                                {"A","Pedir impresora#1", "A", "D", "B", "A", "Pedir impresora#3","Usar impresora#3", "Devolver impresora#3","C", "B", "A", "C", "B", "A", "C", "Usar impresora#1", "Pedir impresora#2", "Usar impresora#2", "Devolver impresora#2", "Devolver impresora#1", "E"}, 
+                                {"P", "P","Pedir impresora#3", "L", "F", "Usar impresora#3", "Devolver impresora#3", "A", "D", "D", "F", "A"}};
+        programas = importarProgs;
+        //importar recursos y anadirlos con su tiempo de espera
+        String[] listaRecursos = {"impresora#1", "impresora#2", "impresora#3", "variableGlobalX", "variableGlobalY"};
+        RCB recurso1 = new RCB(listaRecursos[0], 4);
+        RCB recurso2 = new RCB(listaRecursos[1], 2);
+        RCB recurso3 = new RCB(listaRecursos[2], 6);
+        RCB recurso4 = new RCB(listaRecursos[3], 3);
+        RCB recurso5 = new RCB(listaRecursos[4], 2);
+        recursos.add(recurso1);
+        recursos.add(recurso2);
+        recursos.add(recurso3);
+        recursos.add(recurso4);
+        recursos.add(recurso5);
+        // Crear procesos y los asigna a memoria
+        PCB proceso0 = new PCB(0, programas[0], 0);
+        PCB proceso1 = new PCB(1, programas[1], 1);
+        PCB proceso2 = new PCB(2, programas[2], 2);
+        procesos.add(proceso0);
+        procesos.add(proceso1);
+        procesos.add(proceso2);
+        crearParticiones();
+        asignarAMemoria(proceso0);
+        asignarAMemoria(proceso1);
+        asignarAMemoria(proceso2);
+        //importar Usuarios y cargarlos en procesos
+        String[] importarUsuarios = {"Matixatim Admin"};
+        for(int i=0; i<importarUsuarios.length; i++){
+            String stringUsuario[] = importarUsuarios[i].split(" ");
+            boolean[] permisosRecursos = devolverPermisosRecursosCasoX(definirPerfil(stringUsuario[1]));
+            boolean[] permisosProgramas = devolverPermisosProgramasCasoX(definirPerfil(stringUsuario[1]));
+            Usuario newUsuario = new Usuario(stringUsuario[0], permisosRecursos, permisosProgramas);
+            usuarios.add(newUsuario);
+        }
+        procesos.get(0).setUsuario(usuarios.get(0));
+        procesos.get(1).setUsuario(usuarios.get(0));
+        procesos.get(2).setUsuario(usuarios.get(0));
+        //grafo 
+        inicializarGrafo();
+    }
 
+    //done
+    public void casosPruebaUsuarios1(){
+        //Importar programas
+        String[][] importarProgs = {{"B","C","A","B","B", "Pedir impresora#1","B", "Usar impresora#1", "Devolver impresora#1", "B", "A", "C"}, 
+                                {"A", "A", "D", "B", "A", "Pedir impresora#3","Usar impresora#3", "Devolver impresora#3"}, 
+                                {"P", "P","Pedir impresora#2", "L", "F", "Usar impresora#2", "Devolver impresora#2", "A", "D", "D", "F", "A"}};
+        programas = importarProgs;
+        //importar recursos y anadirlos con su tiempo de espera
+        String[] listaRecursos = {"impresora#1", "impresora#2", "impresora#3"};
+        RCB recurso1 = new RCB(listaRecursos[0], 4);
+        RCB recurso2 = new RCB(listaRecursos[1], 2);
+        RCB recurso3 = new RCB(listaRecursos[2], 6);
+        recursos.add(recurso1);
+        recursos.add(recurso2);
+        recursos.add(recurso3);
+        // Crear procesos y los asigna a memoria
+        PCB proceso0 = new PCB(0, programas[0], 0);
+        PCB proceso1 = new PCB(1, programas[1], 1);
+        PCB proceso2 = new PCB(2, programas[2], 2);
+        PCB proceso3 = new PCB(3, programas[1], 1);
+        PCB proceso4 = new PCB(4, programas[0], 0);
+        PCB proceso5 = new PCB(5, programas[2], 2);
+        PCB proceso6 = new PCB(6, programas[2], 2);
+        procesos.add(proceso0);
+        procesos.add(proceso1);
+        procesos.add(proceso2);
+        procesos.add(proceso3);
+        procesos.add(proceso4);
+        procesos.add(proceso5);
+        procesos.add(proceso6);
+        crearParticiones();
+        asignarAMemoria(proceso0);
+        asignarAMemoria(proceso1);
+        asignarAMemoria(proceso2);
+        asignarAMemoria(proceso3);
+        asignarAMemoria(proceso4);
+        asignarAMemoria(proceso5);
+        asignarAMemoria(proceso6);
+        //importar Usuarios y cargarlos en procesos
+        String[] importarUsuarios = {"Matixatim Admin", "Inaki.exe User", "GL Guest", "Caffa Guest"};
+        for(int i=0; i<importarUsuarios.length; i++){
+            String stringUsuario[] = importarUsuarios[i].split(" ");
+            boolean[] permisosRecursos = devolverPermisosRecursosCasoX(definirPerfil(stringUsuario[1]));
+            boolean[] permisosProgramas = devolverPermisosProgramasCasoX(definirPerfil(stringUsuario[1]));
+            Usuario newUsuario = new Usuario(stringUsuario[0], permisosRecursos, permisosProgramas);
+            usuarios.add(newUsuario);
+        }
+        procesos.get(0).setUsuario(usuarios.get(0));
+        procesos.get(1).setUsuario(usuarios.get(1));
+        procesos.get(2).setUsuario(usuarios.get(2));
+        procesos.get(3).setUsuario(usuarios.get(2));
+        procesos.get(4).setUsuario(usuarios.get(3));
+        procesos.get(5).setUsuario(usuarios.get(2));
+        procesos.get(6).setUsuario(usuarios.get(2));
+        //grafo 
+        inicializarGrafo();
+    }
+
+    //done
+    public void casosPruebaAsignacionDeMemoria1(){
+        //Importar programas
+        String[][] importarProgs = {{"A","B","C","D"},
+                                    {"B", "A", "D"},
+                                    {"C", "E", "A"},
+                                    {"D", "A", "B"},
+                                    {"E", "A", "D"}};
+        programas = importarProgs;
+        //importar recursos y anadirlos con su tiempo de espera
+        //No hay recursos
+        // Crear procesos y los asigna a memoria
+        PCB proceso0 = new PCB(0, programas[0], 0);
+        PCB proceso1 = new PCB(1, programas[1], 1);
+        PCB proceso2 = new PCB(2, programas[2], 2);
+        PCB proceso3 = new PCB(3, programas[0], 3);
+        PCB proceso4 = new PCB(4, programas[1], 2);
+        PCB proceso5 = new PCB(5, programas[2], 2);
+        PCB proceso6 = new PCB(6, programas[0], 0);
+        PCB proceso7 = new PCB(7, programas[1], 1);
+        PCB proceso8 = new PCB(8, programas[2], 4);
+        PCB proceso9 = new PCB(9, programas[0], 4);
+        PCB proceso10 = new PCB(10, programas[1], 2);
+        PCB proceso11= new PCB(11, programas[2], 2);
+        procesos.add(proceso0);
+        procesos.add(proceso1);
+        procesos.add(proceso2);
+        procesos.add(proceso3);
+        procesos.add(proceso4);
+        procesos.add(proceso5);
+        procesos.add(proceso6);
+        procesos.add(proceso7);
+        procesos.add(proceso8);
+        procesos.add(proceso9);
+        procesos.add(proceso10);
+        procesos.add(proceso11);
+        crearParticiones();
+        asignarAMemoria(proceso0);
+        asignarAMemoria(proceso1);
+        asignarAMemoria(proceso2);
+        asignarAMemoria(proceso3);
+        asignarAMemoria(proceso4);
+        asignarAMemoria(proceso5);
+        asignarAMemoria(proceso6);
+        asignarAMemoria(proceso7);
+        asignarAMemoria(proceso8);
+        asignarAMemoria(proceso9);
+        asignarAMemoria(proceso10);
+        asignarAMemoria(proceso11);
+        //importar Usuarios y cargarlos en procesos
+        String[] importarUsuarios = {"Matixatim Admin"};
+        for(int i=0; i<importarUsuarios.length; i++){
+            String stringUsuario[] = importarUsuarios[i].split(" ");
+            boolean[] permisosRecursos = devolverPermisosRecursosCasoX(definirPerfil(stringUsuario[1]));
+            boolean[] permisosProgramas = devolverPermisosProgramasCasoX(definirPerfil(stringUsuario[1]));
+            Usuario newUsuario = new Usuario(stringUsuario[0], permisosRecursos, permisosProgramas);
+            usuarios.add(newUsuario);
+        }
+        procesos.get(0).setUsuario(usuarios.get(0));
+        procesos.get(1).setUsuario(usuarios.get(0));
+        procesos.get(2).setUsuario(usuarios.get(0));
+        procesos.get(3).setUsuario(usuarios.get(0));
+        procesos.get(4).setUsuario(usuarios.get(0));
+        procesos.get(5).setUsuario(usuarios.get(0));
+        procesos.get(6).setUsuario(usuarios.get(0));
+        procesos.get(7).setUsuario(usuarios.get(0));
+        procesos.get(8).setUsuario(usuarios.get(0));
+        procesos.get(9).setUsuario(usuarios.get(0));
+        procesos.get(10).setUsuario(usuarios.get(0));
+        procesos.get(11).setUsuario(usuarios.get(0));
+        //grafo 
+        inicializarGrafo();
+    }
+
+    //done
+    public void casosPruebaAsignacionDeMemoria2(){
+        //Importar programas
+        String[][] importarProgs = {{"A","B","C","D"}};
+        programas = importarProgs;
+        //importar recursos y anadirlos con su tiempo de espera
+        //no hay recursos
+        // Crear procesos y los asigna a memoria
+        PCB proceso0 = new PCB(0, programas[0], 0);
+        PCB proceso1 = new PCB(1, programas[0], 0);
+        PCB proceso2 = new PCB(2, programas[0], 0);
+        PCB proceso3 = new PCB(3, programas[0], 0);
+        PCB proceso4 = new PCB(4, programas[0], 0);
+        PCB proceso5 = new PCB(5, programas[0], 0);
+        PCB proceso6 = new PCB(6, programas[0], 0);
+        PCB proceso7 = new PCB(7, programas[0], 0);
+        PCB proceso8 = new PCB(8, programas[0], 0);
+        PCB proceso9 = new PCB(9, programas[0], 0);
+        PCB proceso10 = new PCB(10, programas[0], 0);
+        procesos.add(proceso0);
+        procesos.add(proceso1);
+        procesos.add(proceso2);
+        procesos.add(proceso3);
+        procesos.add(proceso4);
+        procesos.add(proceso5);
+        procesos.add(proceso6);
+        procesos.add(proceso7);
+        procesos.add(proceso8);
+        procesos.add(proceso9);
+        procesos.add(proceso10);
+        crearParticiones();
+        asignarAMemoria(proceso0);
+        asignarAMemoria(proceso1);
+        asignarAMemoria(proceso2);
+        asignarAMemoria(proceso3);
+        asignarAMemoria(proceso4);
+        asignarAMemoria(proceso5);
+        asignarAMemoria(proceso6);
+        asignarAMemoria(proceso7);
+        asignarAMemoria(proceso8);
+        asignarAMemoria(proceso9);
+        asignarAMemoria(proceso10);
+        //importar Usuarios y cargarlos en procesos
+        String[] importarUsuarios = {"wideMatixatim Admin"};
+        for(int i=0; i<importarUsuarios.length; i++){
+            String stringUsuario[] = importarUsuarios[i].split(" ");
+            boolean[] permisosRecursos = devolverPermisosRecursosCasoX(definirPerfil(stringUsuario[1]));
+            boolean[] permisosProgramas = devolverPermisosProgramasCasoX(definirPerfil(stringUsuario[1]));
+            Usuario newUsuario = new Usuario(stringUsuario[0], permisosRecursos, permisosProgramas);
+            usuarios.add(newUsuario);
+        }
+        for(int i=0; i<=10; i++){
+            procesos.get(i).setUsuario(usuarios.get(0));
+        }
+        //grafo 
+        inicializarGrafo();
+    }
+
+    public void casosPruebaGeneral(){
+        //Importar programas
+        String[][] importarProgs = {{"Pedir impresora#2","B","C","A","B","B", "Pedir impresora#1", "Usar impresora#2", "Usar impresora#1", "Devolver impresora#1", "Devolver impresora#2", "B", "A", "C"}, 
+                                {"A","Pedir impresora#1", "A", "D", "B", "A", "Pedir impresora#3","Usar impresora#3", "Devolver impresora#3","C", "B", "A", "C", "B", "A", "C", "Usar impresora#1", "Pedir impresora#2", "Usar impresora#2", "Devolver impresora#2", "Devolver impresora#1", "E"}, 
+                                {"P", "P","Pedir impresora#3", "L", "F", "Usar impresora#3", "Devolver impresora#3", "A", "D", "D", "F", "A"}};
+        programas = importarProgs;
+        //importar recursos y anadirlos con su tiempo de espera
+        String[] listaRecursos = {"impresora#1", "impresora#2", "variableGlobalX"};
+        RCB recurso1 = new RCB(listaRecursos[0], 4);
+        RCB recurso2 = new RCB(listaRecursos[1], 2);
+        RCB recurso3 = new RCB(listaRecursos[2], 6);
+        recursos.add(recurso1);
+        recursos.add(recurso2);
+        recursos.add(recurso3);
+        // Crear procesos y los asigna a memoria
+        PCB proceso0 = new PCB(0, programas[0], 0);
+        PCB proceso1 = new PCB(1, programas[1], 1);
+        PCB proceso2 = new PCB(2, programas[2], 2);
+        PCB proceso3 = new PCB(3, programas[1], 1);
+        PCB proceso4 = new PCB(4, programas[0], 0);
+        PCB proceso5 = new PCB(5, programas[2], 2);
+        PCB proceso6 = new PCB(6, programas[2], 2);
+        procesos.add(proceso0);
+        procesos.add(proceso1);
+        procesos.add(proceso2);
+        procesos.add(proceso3);
+        procesos.add(proceso4);
+        procesos.add(proceso5);
+        procesos.add(proceso6);
+        crearParticiones();
+        asignarAMemoria(proceso0);
+        asignarAMemoria(proceso1);
+        asignarAMemoria(proceso2);
+        asignarAMemoria(proceso3);
+        asignarAMemoria(proceso4);
+        asignarAMemoria(proceso5);
+        asignarAMemoria(proceso6);
+        //importar Usuarios y cargarlos en procesos
+        String[] importarUsuarios = {"Matixatim Admin", "Inaki.exe User", "GL Guest", "Caffa Admin"};
+        for(int i=0; i<importarUsuarios.length; i++){
+            String stringUsuario[] = importarUsuarios[i].split(" ");
+            boolean[] permisosRecursos = devolverPermisosRecursosCasoX(definirPerfil(stringUsuario[1]));
+            boolean[] permisosProgramas = devolverPermisosProgramasCasoX(definirPerfil(stringUsuario[1]));
+            Usuario newUsuario = new Usuario(stringUsuario[0], permisosRecursos, permisosProgramas);
+            usuarios.add(newUsuario);
+        }
+        procesos.get(0).setUsuario(usuarios.get(0));
+        procesos.get(1).setUsuario(usuarios.get(1));
+        procesos.get(2).setUsuario(usuarios.get(2));
+        procesos.get(3).setUsuario(usuarios.get(2));
+        procesos.get(4).setUsuario(usuarios.get(3));
+        procesos.get(5).setUsuario(usuarios.get(0));
+        procesos.get(6).setUsuario(usuarios.get(3));
+        //grafo 
+        inicializarGrafo();
+    }
 
     public boolean[] devolverPermisosProgramasCasoX(Perfiles p){
         boolean[] permisosProgramas = new boolean[programas.length];
--- main.java
+++ main.java
@@ -11,15 +11,15 @@ class main{
         Sistema sistema = Sistema.Current();
         Scanner scanner = new Scanner(System.in);
         boolean exit = false;
-        //ponerle colores
+
         System.out.println(colores.ANSI_WHITE_BOLD + " Simulador de un sistema operativo" + colores.ANSI_RESET);
         System.out.println(colores.ANSI_WHITE + "Bienvenido a la interfaz por consola" + colores.ANSI_RESET);
         System.out.println("Hecho por " + colores.ANSI_WHITE_BOLD + "Inaki etchegaray, Matias Gonzalez y Gaston Landeira" + colores.ANSI_RESET);
         while(!exit){
             System.out.println("-------------------------------------------------------------");
-            System.out.println("Inserte el numero de opcion que desea elegir");
+            System.out.println("Inserte el número de opción que desea elegir");
             System.out.println("1 -> Casos de prueba");
-            System.out.println("2 -> Ayuda(Descrpicion de casos de ayuda)");
+            System.out.println("2 -> Ayuda(Descripición de casos de ayuda)");
             System.out.println("3 -> Salir del programa");
             //colores.ANSI_RED + "No hay mas procesos por ejecutar..." + colores.ANSI_RESET
             int entrada = scanner.nextInt();
@@ -43,10 +43,16 @@ class main{
         boolean exitPrueba = false;
         while(!exitPrueba){
             System.out.println("");
-            System.out.println("Inserte el numero de opcion que desea elegir");
-            System.out.println("1 -> Prueba deadlocks1");
-            System.out.println("2 -> Prueba deadlocks2");
-            System.out.println("3 -> Salir de los cassos de prueba");
+            System.out.println("Inserte el numero de opción que desea elegir");
+            System.out.println("1 -> Prueba deadlocks Básica");
+            System.out.println("2 -> Prueba deadlocks Avanzada 1");
+            System.out.println("3 -> Prueba deadlocks Avanzada 2");
+            System.out.println("4 -> Prueba usuarios");
+            System.out.println("5 -> Prueba asignación de memoria Básica");
+            System.out.println("6 -> Prueba asignación de memoria Avanzada");
+            System.out.println("7 -> Prueba General");
+            System.out.println("8 -> Salir de los casos de prueba");
+            System.out.println("");
 
             int entrada = scanner.nextInt();
             
@@ -58,6 +64,21 @@ class main{
                     sistema.casosPruebaDeadlocks2();
                 break;
                 case 3:
+                    sistema.casosPruebaDeadlocks3();
+                break;
+                case 4:
+                    sistema.casosPruebaUsuarios1();
+                break;
+                case 5:
+                    sistema.casosPruebaAsignacionDeMemoria1();
+                break;
+                case 6:
+                    sistema.casosPruebaAsignacionDeMemoria2();
+                break;
+                case 7:
+                    sistema.casosPruebaGeneral();
+                break;
+                case 8:
                     exitPrueba = true;
                 break;
             }
@@ -79,44 +100,45 @@ class main{
         boolean exitAyuda = false;
         while(!exitAyuda){
             System.out.println("");
-            System.out.println("Inserte el numero de opcion que desea elegir");
-            System.out.println("1 -> casosPruebaDeadlocks1 explicacion");
-            System.out.println("2 -> casosPruebaDeadlocks2 explicacion");
-            System.out.println("3 -> Salir de la ayuda");
+            System.out.println("Inserte el numero de opción que desea elegir");
+            System.out.println("1 -> Prueba deadlocks Básica explicación");
+            System.out.println("2 -> Prueba deadlocks Avanzada 1 explicación");
+            System.out.println("3 -> Prueba deadlocks Avanzada 2 explicación");
+            System.out.println("4 -> Prueba usuarios explicación");
+            System.out.println("5 -> Prueba asignación de memoria Básica explicación");
+            System.out.println("6 -> Prueba asignación de memoria Avanzada explicación");
+            System.out.println("7 -> Prueba General explicación");
+            System.out.println("8 -> Salir de la sección de ayuda");
+            System.out.println("");
 
             int entrada = scanner.nextInt();
             
             switch (entrada){
                 case 1:
-                    
+                    System.out.println("");
                 break;
                 case 2:
-                    
+                    System.out.println("");
                 break;
                 case 3:
+                    System.out.println("");
+                break;
+                case 4:
+                    System.out.println("");
+                break;
+                case 5:
+                    System.out.println("");
+                break;
+                case 6:
+                    System.out.println("");
+                break;
+                case 7:
+                    System.out.println("");
+                break;
+                case 8:
                     exitAyuda = true;
                 break;
             }
         }
     }
-
-    
-    /**
-     * Logica de usuario:
-     * El usuario tiene permisos a procesos y recursos, para esto necesitamos
-     * Matriz de bools (True = acceso, False = no) de usuarios y recursos
-     * Matriz de bools "" de usuarios y procesos
-     * 
-     * Uso de las matrices:
-     * Cuando vas a ejecutar un proceso ver en la posicion M[u][p] si el usuario tiene permiso, si no tiene printear "No tiene acceso a este proceso" y seguir con el proximo en RR
-     * Cuando vas adentro de un proceso vas a PEDIR un recurso fijarse si el usuario tiene permiso para utilizar el recurso, Si lo tiene avanza normal, si no matar proceso
-     * 
-     * IDEAS:
-     * Funcion en sistema permisoRecurso(usuario, recurso)
-     * Funcion en sistema permisoProceso(usuario, proceso)
-     * Funcion en PCB matarProceso que lo termine y lo saque del scheduller
-     * Fijarse si usuario tiene permiso afuera del PCB, si no lo tiene no ejecutar el proceso directamente
-     * Fijarse si el usuario tiene permiso para usar el recurso, si no lo tiene, llamar a matarProceso, y sacar proceso de Round Robin 
-     */
-
 }
